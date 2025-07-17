a50270e5fc45d29eaf4f882d646f048e7d56af2c
ratthanun1008-1 <ratthanun.ruk@rmutto.ac.th>
2025-07-17 12:34:05 +0700

    Lab02

---
+++ app/routes/Ozone-Lab02.tsx
@@ -0,0 +1,14 @@
+function MyProfile () {
+    return (
+        <div>
+            <p className="text-2x1">Ozone Profile</p>
+            <img src="/images/Apple-logo.png" className="w-1/4"/>
+            <p className="text-lg">
+                Name: Ratthanun Rukkarn<br />
+                Student ID: 026730491008-1 <br />
+                </p>
+        </div>
+    );
+}
+
+export default MyProfile;
